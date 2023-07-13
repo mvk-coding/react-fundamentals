028bd7a864efcc94b98770fb847fdabcea046b89
Manon <mahvankeulen@gmail.com>
2023-07-13 15:17:17 +0200

    added my solutions for exercise 4-7

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,19 +4,87 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 🐨 add a className prop to each div and apply the correct class names
-// based on the text content
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-// 💰 each of the elements should have the "box" className applied
+// Basic
+// const smallBox = (
+//   <div
+//     className="box box--small"
+//     style={{backgroundColor: 'lightBlue', fontStyle: 'italic'}}
+//   >
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = (
+//   <div
+//     className="box box--medium"
+//     style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+//   >
+//     medium pink box
+//   </div>
+// )
+// const largeBox = (
+//   <div
+//     className="box box--large"
+//     style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+//   >
+//     large orange box
+//   </div>
+// )
 
-// 🐨 add a style prop to each div so their background color
-// matches what the text says it should be
-// 🐨 also use the style prop to make the font italic
-// 💰 Here are available style attributes: backgroundColor, fontStyle
+//Extra credit 1
+// function Box({className = '', style, ...otherProps}) {
+//   return (
+//     <div
+//       className={`box ${className}`.trim()}
+//       style={{fontStyle: 'italic', ...style}}
+//       {...otherProps}
+//     />
+//   )
+// }
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// const smallBox = (
+//   <Box className="box--small" style={{backgroundColor: 'lightBlue'}}>
+//     small lightblue box
+//   </Box>
+// )
+// const mediumBox = (
+//   <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+//     medium pink box
+//   </Box>
+// )
+// const largeBox = (
+//   <Box className="box--large" style={{backgroundColor: 'orange'}}>
+//     large orange box
+//   </Box>
+// )
+
+// Extra credit 2
+function Box({className="", size, style, ...otherProps}) {
+  if (size) className += `box--${size}`
+
+  return (
+    <div
+      className={`box box--${size}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
+
+const smallBox = (
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box size="large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
 
 function App() {
   return (
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,26 +4,59 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  //Extra Credit 2
+  // const [error, setError] = React.useState(null)
 
-  // 🐨 add the onSubmit handler to the <form> below
+  //Extra Credit 3
+  const [username, setUsername] = React.useState('')
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  //Extra Credit 1
+  const usernameRef = React.useRef(null)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    //Basic
+    //const value = event.target.elements.username.value
+
+    //Extra Credit 1
+    // const value = usernameRef.current.value
+    // onSubmitUsername(value)
+
+    //Extra Credit 3
+    onSubmitUsername(username)
+  }
+
+  //Extra Credit 2
+  // const handleChange = event => {
+  //   const value = event.target.value
+  //   const isValid = value === value.toLowerCase()
+  //   setError(isValid ? null : 'Username must be lower case')
+  // }
+
+  //Extra Credit 3
+  const handleChange = event => {
+    const value = event.target.value
+    setUsername(value.toLowerCase())
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username:</label>
+        <input
+          id="username"
+          type="text"
+          ref={usernameRef}
+          onChange={handleChange}
+          value={username}
+        />
+        {/* Extra Credit 2 */}
+        {/* {error && (
+          <div role="alert" style={{color: 'red'}}>
+            {error}
+          </div>
+        )} */}
       </div>
       <button type="submit">Submit</button>
     </form>
